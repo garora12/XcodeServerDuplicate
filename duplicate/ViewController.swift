1522fdbcaab994e7e89637a8fb956262dbc1baab
K P Singh <kpsingh@kanwarpal-iphone.local>
2018-11-30 12:15:37 +0530

    Initial Commit

---
+++ duplicate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  duplicate
+//
+//  Created by K P Singh on 11/30/18.
+//  Copyright © 2018 K P Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
